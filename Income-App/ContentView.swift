6533cc2f98880988a4d216ace183f99077f13e18
zoli0829 <zoli0829@gmail.com>
2025-09-26 17:44:21 +0100

    Initial commit

---
+++ Income-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Income-App
+//
+//  Created by Zoltan Vegh on 26/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Income-App/Income_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Income_AppApp.swift
+//  Income-App
+//
+//  Created by Zoltan Vegh on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Income_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
